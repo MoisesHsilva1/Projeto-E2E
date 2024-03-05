d964db85a23e933223970532f3fd60a7915bbde6
MoisésHsilva1 <moihgm67@gmail.com>
2024-03-04 21:23:10 -0300

    atualizando projteo

--- cypress/e2e/LoginCorreto.cy.js
+++ cypress/e2e/LoginCorreto.cy.js
@@ -1,10 +1,9 @@
 describe('Pagina de login', () => {
-    it('Preecher o campo de login corretamente', () => {
-        cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
 
-        cy.get('[data-test="loginUserName"]').type('catarinatn');
-        cy.get('[data-test="loginPassword"]').type('12345678');
-        
-        cy.contains('button', 'login').click();
+    beforeEach(()  => {
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home') })
+
+    it('Preecher o campo de login corretamente', () => {
+      cy.login('pedroT', '12345678')
     })
 })
--- cypress/e2e/cadastroCorreto.cy.js
+++ cypress/e2e/cadastroCorreto.cy.js
@@ -2,9 +2,9 @@ describe('Pagina de cadastro', () => {
   it('Preencher os campos dos formularios corretamente para cadastrar um novo usuario', () => {
     cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
     cy.contains('a', 'Register now').click();
-    cy.get('[data-test="email"]').type('catarina@email.com.br');
-    cy.get('[data-test="fullName"]').type('Catarina Pessoa ');
-    cy.get('[data-test="registerUserName"]').type('catarinatn');
+    cy.get('[data-test="email"]').type('pedro@email.com.br');
+    cy.get('[data-test="fullName"]').type('pedro teste ');
+    cy.get('[data-test="registerUserName"]').type('pedro123');
     cy.get('[data-test="registerPassword"]').type('12345678');
     cy.contains('button', 'Register').click();
   })
